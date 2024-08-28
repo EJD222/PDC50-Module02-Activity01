e3a381a625b4b266800993b0ce5a81d0c42c20b6
EJD222 <eljdiaz500@gmail.com>
2024-08-28 10:56:38 +0800

    Add project files.

---
+++ Module02Activity01/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Module02Activity01
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Nunito-Regular", "NunitoRegular");
+                    fonts.AddFont("Nunito-SemiBold", "NunitoSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
